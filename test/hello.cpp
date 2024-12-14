1d17434a54aec85e35bdef641a8081cfce7b6ceb
billal297222 <billal297222@gmail.com>
2024-12-14 14:43:10 +0600

    first commit

---
+++ test/hello.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define all(x) x.begin(),x.end() //is_sorted(all(x))
+#define pb push_back(x)
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a / gcd(a, b)) * b
+
+int main(){
+  int t; 
+  cin>>t;
+  while(t--)
+  {
+ 
+  }
+  return 0;
+}
